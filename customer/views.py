141e5d53c00c62ff35abfccc4270baecc821854c
Prince Johnson <prince.pangaea@gmail.com>
2024-05-12 12:48:27 -0500

    misc

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from .models import Customer
 from address.models import Address
+from .forms import CustomerForm
 
 
 # Create your views here.
@@ -9,38 +10,11 @@ def CustomerList(request):
     context = {'customer':customer}
     return render(request, 'customer/customerlist.html', context)
 
-def CustomerForm(request):
-    context = {}
-    return render(request, 'customer/customerform.html', context)
-
 def CustomerDetail(request, cpk):
     customer_detail = Customer.objects.get(id=cpk)
     custaddresses = Address.objects.filter(customer__id=cpk)
     context = {'customer_detail':customer_detail, 'cpk':cpk, 'custaddresses':custaddresses }
     return render(request, 'customer/customerdetail.html', context)
-
-'''
-def CustomerDetail(request, cpk):
-    customer_detail = Customer.objects.get(id=cpk)
-    custaddresses = Address.objects.get(customer=customer_detail)
-    context = {'customer_detail':customer_detail, 'cpk':cpk, 'custaddresses':custaddresses }
-    return render(request, 'customer/customerdetail.html', context)
-'''
-'''
-def CustomerDetail(request, cpk):
-    customer = Customer.objects.get(Customer.id)
-    customeraddresses = Address.objects.filter(customer)
-    customer_detail = Customer.objects.get(id=cpk)
-    context = {'customer_detail':customer_detail, 'cpk':cpk, 'customeradresses':customeraddresses }
-    return render(request, 'customer/customerdetail.html', context)
-'''
-'''
-# this one works
-def CustomerDetail(request, cpk):
-    customer_detail = Customer.objects.get(id=cpk)
-    context = {'customer_detail':customer_detail, 'cpk':cpk }
-    return render(request, 'customer/customerdetail.html', context)
-'''
 
 def AddCustomer(request):
     form = CustomerForm(request.POST or None)
